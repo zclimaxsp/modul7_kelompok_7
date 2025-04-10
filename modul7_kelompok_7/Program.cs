f1e1d83ebd1d69cda0496f6840ce9fac5878b0e0
alya <alyarabani33@gmail.com>
2025-04-10 17:28:49 +0700

    menambahkan class DataMahasiswa2311104076

--- modul7_kelompok_7/Program.cs
+++ modul7_kelompok_7/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class Address
+{
+    public string? streetAddress { get; set; }
+    public string? city { get; set; }
+    public string? state { get; set; }
+}
+
+public class Course
+{
+    public string? code { get; set; }
+    public string? name { get; set; }
+}
+
+public class DataMahasiswa2311104076
+{
+    public string? firstName { get; set; }
+    public string? lastName { get; set; }
+    public string? gender { get; set; }
+    public int age { get; set; }
+    public Address? address { get; set; }
+    public List<Course>? courses { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ReadJSON();
+    }
+
+    static void ReadJSON()
+    {
+        string filePath = "jurnal7_1_2311104076.json";
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File '{filePath}' tidak ditemukan.");
+            return;
+        }
+
+        string jsonData = File.ReadAllText(filePath);
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var data = JsonSerializer.Deserialize<DataMahasiswa2311104076>(jsonData, options);
+
+        Console.WriteLine("=== Data Mahasiswa ===");
+        Console.WriteLine($"Nama       : {data?.firstName} {data?.lastName}");
+        Console.WriteLine($"Gender     : {data?.gender}");
+        Console.WriteLine($"Usia       : {data?.age}");
+        Console.WriteLine($"Alamat     : {data?.address?.streetAddress}, {data?.address?.city}, {data?.address?.state}");
+
+        Console.WriteLine("Mata Kuliah:");
+        if (data?.courses != null)
+        {
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine($"  - {course.code}: {course.name}");
+            }
+        }
+    }
+}
--- modul7_kelompok_7/obj/Release/net7.0/modul7_kelompok_7.AssemblyInfo.cs
+++ modul7_kelompok_7/obj/Release/net7.0/modul7_kelompok_7.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul7_kelompok_7")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8390f80f670121a80e3dcd839dd3108073464d")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul7_kelompok_7")]
